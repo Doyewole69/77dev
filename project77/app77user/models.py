9f2b7dc5f02c3d78fdc189126fb9ed2667d0c385
Doyewole69 <102881778+Doyewole69@users.noreply.github.com>
2022-09-06 00:14:01 +0100

    update

--- project77/app77user/models.py
+++ project77/app77user/models.py
@@ -1,13 +1,18 @@
 from audioop import maxpp
 from django.db import models
 from django.contrib.auth.models import AbstractUser,UserManager
-
+from decimal import Decimal
+from .managers import UserManager
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
 class CustomUserManager(UserManager):
     pass
 
 class CustomUser(AbstractUser):
     objects = CustomUserManager()
+
+class AccountType(models.Model):
+    name = models.CharField(max_length=128)
 
 class ForexDenModel(models.Model):
     Balance = models.CharField(max_length=10)
